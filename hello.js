f80cbb1b6ba5a33be5d6c7f2254b4d3b8bcb5a98
Devashish Saxena <saxenad013@gmail.com>
2026-06-24 18:04:57 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,15 @@
+// My first JavaScript file
+
+console.log("Hello! I am learning MERN Stack!");
+
+ 
+
+let studentName = "Ravi";
+
+let course = "MERN Fundamentals";
+
+ 
+
+console.log("Student Name: " + studentName);
+
+console.log("Course: " + course);
