05852be6ef4f6c54e8b3c36baf7d39c0905a58d4
Devashish Saxena <saxenad013@gmail.com>
2026-06-24 18:02:09 +0530

    Add files via upload

---
+++ student.js
@@ -0,0 +1,16 @@
+// Name: Devashish Saxena
+// Date: June 23, 2026
+
+// Storing student information in separate variables
+let fullName = "Devashish Saxena";
+let age = 21; // You can update this to your actual age
+let branch = "Computer Engineering"; 
+let favouriteSubject = "Web Development";
+let githubUsername = "devashish_codes"; // Update with your actual GitHub username
+
+// Displaying the information using console.log()
+console.log("Student Name: " + fullName);
+console.log("Age: " + age);
+console.log("Branch: " + branch);
+console.log("Favourite Subject: " + favouriteSubject);
+console.log("GitHub: " + githubUsername);
